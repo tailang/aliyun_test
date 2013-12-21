27dd249fe212c97715b00e66869dd28ae570433c
tailang <caopingcpu@163.com>
2013-12-21 18:17:44 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aliyun::Application.config.secret_token = '2ee1caf8c7e61cb392c286317c598a51786a8b9c648e20ad3439975e78bd7076fb6230f227f8a6a0683ad5699341f69d9d404f96e0166f405f5ff1825d5ef2b1'
