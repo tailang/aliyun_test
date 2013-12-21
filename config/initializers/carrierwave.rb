f03b35b681eacc5b523b8ab8a759e094db2efca7
tailang <caopingcpu@163.com>
2013-12-21 18:57:29 +0800

    2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
   # 你需要在 Aliyum OSS 上面提前创建一个 Bucket
   config.aliyun_bucket = "guanjieba"
   # 是否使用内部连接，true - 使用 Aliyun 局域网的方式访问  false - 外部网络访问
-  config.aliyun_internal = false
+  config.aliyun_internal = true
   # 配置存储的地区数据中心，默认: cn-hangzhou
   # config.aliyun_area = "cn-hangzhou" 
   # 使用自定义域名，设定此项，carrierwave 返回的 URL 将会用自定义域名
