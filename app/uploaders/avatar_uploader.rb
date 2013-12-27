697a084696d0f2d1a324ba90eadd8a362413fe96
tailang <caopingcpu@163.com>
2013-12-27 15:52:27 +0800

    second

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,7 +7,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage Settings.which_storage
+  #storage Settings.which_storage
   
   process :time_start
   # storage :fog
